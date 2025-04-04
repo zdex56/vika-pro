6646aef71c90ed4779c11896edb38e8fcb5158b9
zdex <zdex@DESKTOP-SK0AJHV>
2025-04-04 17:41:33 +0500

    sdelal

--- vika.cpp
+++ vika.cpp
@@ -1,6 +1,40 @@
-
 #include <iostream>
-int main() {
-    std::cout << ";ad;adl" << std::endl;
-    return 0;
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Contact {
+    string name;
+    string phone;
+    string email;
+};
+
+vector<Contact> contacts;
+
+void addContact() {
+    Contact newContact;
+    cout << "как тя звать ";
+    cin >> newContact.name;
+    cout << "циферки накдиктуй  ";
+    cin >> newContact.phone;
+    cout << "эмаил дай  ";
+    cin >> newContact.email;
+    contacts.push_back(newContact);
+    cout << "я тебя запомнил " << endl;
 }
+
+void deleteContact() {
+    string name;
+    cout << "ведите имя для БАНА ";
+    cin >> name;
+    for (auto it = contacts.begin(); it != contacts.end(); ++it) {
+        if (it->name == name) {
+            contacts.erase(it);
+            cout << "ЗАБАНЕН" << endl;
+            return;
+        }
+    }
+    cout << "контакт не найден" << endl;
+}
+
+// это часть ИльясА Абулхаиров  крч я сделал ввод и удаление
