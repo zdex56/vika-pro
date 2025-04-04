9456e2e925150575426e2129f3c9bda4f02e0deb
zdex <zdex@DESKTOP-SK0AJHV>
2025-04-04 17:43:06 +0500

    lol

--- vika.cpp
+++ vika.cpp
@@ -13,28 +13,28 @@ vector<Contact> contacts;
 
 void addContact() {
     Contact newContact;
-    cout << "как тя звать ";
+    cout << "name tvoi ";
     cin >> newContact.name;
-    cout << "циферки накдиктуй  ";
+    cout << "nomer telefona ";
     cin >> newContact.phone;
-    cout << "эмаил дай  ";
+    cout << "email  ";
     cin >> newContact.email;
     contacts.push_back(newContact);
-    cout << "я тебя запомнил " << endl;
+    cout << "ya teba sapomnil " << endl;
 }
 
 void deleteContact() {
     string name;
-    cout << "ведите имя для БАНА ";
+    cout << "name dai dla bana";
     cin >> name;
     for (auto it = contacts.begin(); it != contacts.end(); ++it) {
         if (it->name == name) {
             contacts.erase(it);
-            cout << "ЗАБАНЕН" << endl;
+            cout << "USER GET BAN EZ LOL " << endl;
             return;
         }
     }
-    cout << "контакт не найден" << endl;
+    cout << "ne naiden " << endl;
 }
 
 // это часть ИльясА Абулхаиров  крч я сделал ввод и удаление
