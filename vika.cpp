5da1e348da88879fa954d2af71110503ff98ff8e
VikaMatova <klopusyk@gmail.com>
2025-04-04 18:20:04 +0500

    Ia DODELALA pochti

--- vika.cpp
+++ vika.cpp
@@ -38,3 +38,65 @@ void deleteContact() {
 }
 
 // это часть ИльясА Абулхаиров  крч я сделал ввод и удаление
+
+
+
+void editContact() {
+    string name;
+    cout << "Vvedite name dlia redaktirovania ";
+    cin >> name;
+    for (auto& contact : contacts) {
+        if (contact.name == name) {
+            cout << "vvedite new name: ";
+            cin >> contact.name;
+            cout << "Vvedite new namber: ";
+            cin >> contact.phone;
+            cout << "Vvedite new pochtu: ";
+            cin >> contact.email;
+            cout << "GOTOVO" << endl;
+            return;
+        }
+    }
+    cout << "nichego ne naydeno" << endl;
+}
+
+
+
+
+void Menu() {
+    cout << "Menu:\n ";
+    cout << "1. Dobavit kontakt, ";
+    cout << "2. Delete kontakt, ";
+    cout << "4. izmenit kontakt, ";
+    cout << "5. Exit ";
+    cout << "Vibrat deystvia: ";
+}
+
+
+int main() {
+    int choice;
+    do {
+        Menu();
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            addContact();
+            break;
+        case 2:
+            deleteContact();
+            break;
+
+        case 3:
+            editContact();
+            break;
+
+        case 4:
+            cout << "Exit" << endl;
+            break;
+        default:
+            cout << "ti chtoto putaesh" << endl;
+        }
+    } while (choice != 4);
+}
+
+//Это часть Матовой Вики
